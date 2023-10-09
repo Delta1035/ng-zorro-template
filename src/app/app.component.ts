21995359bb6134ba4db9b6bc9d770cdf5997c679
chongyang.zheng <chongyang.zheng@jmalltech.com>
2023-10-09 09:58:49 +0800

    init🎉: 引入ngzorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NzButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
